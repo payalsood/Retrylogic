7febdf0b7ee3dc6bb7535ca5d3adda0dbd973315
Payal Sood <liferockson27@gmail.com>
2020-05-24 16:12:32 +0530

    Retry logic

---
+++ src/test/java/mytests/MyTests.java
@@ -0,0 +1,20 @@
+package mytests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTests {
+
+	@Test
+	public void Test1(){
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void Test2(){
+		Assert.assertEquals(false, true);
+	}
+	
+}
